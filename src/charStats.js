8a5ddc9966783fd7513226bdfc4d51cc369efbca
ejohnston <ejohnston86@gmail.com>
2017-11-06 13:28:55 -0500

    createMap ok

---
+++ src/charStats.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+const CharStats = (props) => {
+  return (
+
+  )
+}
+
+export default 'charStats';
--- src/index.js
+++ src/index.js
@@ -1,197 +1,125 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.min.css';
-import{ ButtonToolbar, MenuItem, DropdownButton } from 'react-bootstrap';
 
-class Box extends React.Component{
-  selectBox = () =>{
-    this.props.selectBox(this.props.row, this.props.col)
+// import './charStats.js';
+// import './weapons.js';
+// import './traps.js';
+// import './baddies';
+
+class Application extends React.Component {
+    constructor(props) {
+    super(props);
+    this.state = {
+      dimensions: 15,
+      maxTunnels: 60,
+      maxLength: 8
+    };
+    this.onClick = this.onClick.bind(this);
+    this.onChange = this.onChange.bind(this);
   }
 
-  render() {
-    return(
-      <div
-        className={this.props.boxClass}
-        id={this.props.id}
-        onClick={this.selectBox}
-      />
-    );
-  }
-}
-
-class Grid extends React.Component{
-  render() {
-    const width = (this.props.cols * 16);
-    var rowsArr = []
-
-    var boxClass = "";
-    for (var i = 0; i < this.props.rows; i++) {
-      for (var j = 0; j < this.props.cols; j++) {
-        let boxId = i + "_" + j;
-
-        boxClass = this.props.gridFull[i][j] ? "box on" : "box off";
-        rowsArr.push(
-          <Box
-            boxClass={boxClass}
-            key={boxId}
-            boxId={boxId}
-            row={i}
-            col={j}
-            selectBox={this.props.selectBox}
-          />
-        );
+  createArray(num, dimensions) {
+    var array = [];
+    for (var i = 0; i < dimensions; i++) {
+      array.push([]);
+      for (var j = 0; j < dimensions; j++) {
+        array[i].push(num);
       }
     }
-
-    return (
-      <div className="grid" style={{width:width}}>
-        {rowsArr}
-      </div>
-    );
-  }
-}
-
-class Buttons extends React.Component{
-
-  handleSelect= (evt) => {
-    this.props.gridSize(evt);
+    return array;
   }
 
-  render() {
-    return (
-      <div className="center">
-        <ButtonToolbar>
-          <button className="btn btn-default" onClick={this.props.playButton}>
-            Play
-          </button>
-
-          <button className="btn btn-default" onClick={this.props.pauseButton}>
-            Pause
-          </button>
-
-          <button className="btn btn-default" onClick={this.props.seed}>
-            Seed
-          </button>
-
-        </ButtonToolbar>
-      </div>
-    )
-  }
-}
-
-class Main extends React.Component{
-  constructor() {
-    super();
-    this.rows = 35;
-    this.cols = 35;
-
-    this.state = {
-      generation:0,
-      gridFull: Array(this.rows).fill().map(()=>Array(this.cols).fill(false))
-    }
-  }
-
-  selectBox = (row, col) => {
-    let gridCopy = arrayClone(this.state.gridFull);
-    gridCopy[row][col] = !gridCopy[row][col];
+  onChange(e) {
     this.setState({
-      gridFull: gridCopy
+      [e.target.name]: this.validator(e.target.value)
     });
   }
 
-  seed = () => {
-		let gridCopy = arrayClone(this.state.gridFull);
-		for (let i = 0; i < this.rows; i++) {
-			for (let j = 0; j < this.cols; j++) {
-				if (Math.floor(Math.random() * this.rows) === 1) {
-					gridCopy[i][j] = true;
-				}
-			}
-		}
-		this.setState({
-			gridFull: gridCopy
-		});
-	}
-
-  playButton = () => {
-    clearInterval(this.intervalId)
-    this.intervalId = setInterval(this.play, this.speed);
-  }
-
-  pauseButton = () => {
-    clearInterval(this.intervalId);
-  }
-
-    gridSize = (size) => {
-      switch (size) {
-        case "1":
-        this.cols = 20;
-        this.rows = 10;
-        break;
-        case "2":
-        this.cols = 50;
-        this.rows = 30;
-        break;
-        default:
-        this.cols = 70;
-        this.rows = 50;
-      }
-      this.clear();
+  validator(x) {
+    let input = Number(x);
+    if (isNaN(input)){
+      return 0;
     }
+    return input;
+  }
 
-    play = () => {
-      let g = this.state.gridFull;
-      let g2 = arrayClone(this.state.gridFull);
-  // Checks 8 neighboring cells
-      for (let i = 0; i < this.rows; i++) {
-        for (let j = 0; j < this.cols; j++) {
-          let count = 0;
-          if (i > 0) if (g[i - 1][j]) count++;
-          if (i > 0 && j > 0) if (g[i - 1][j - 1]) count++;
-          if (i > 0 && j < this.cols - 1) if (g[i - 1][j + 1]) count++;
-          if (j < this.cols - 1) if (g[i][j + 1]) count++;
-          if (j > 0) if (g[i][j - 1]) count++;
-          if (i < this.rows - 1) if (g[i + 1][j]) count++;
-          if (i < this.rows - 1 && j > 0) if (g[i + 1][j - 1]) count++;
-          if (i < this.rows - 1 && this.cols - 1) if (g[i + 1][j + 1]) count++;
-          if (g[i][j] && (count < 2 || count > 3)) g2[i][j] = false;
-          if (!g[i][j] && count === 3) g2[i][j] = true;
+  //lets create a randomly generated map for our dungeon crawler
+  createMap() {
+    let dimensions = this.state.dimensions, // width and height of the map
+      maxTunnels = this.state.maxTunnels, // max number of tunnels possible
+      maxLength = this.state.maxLength, // max length each tunnel can have
+      map = this.createArray(1, dimensions), // create a 2d array full of 1's
+      currentRow = Math.floor(Math.random() * dimensions), // our current row - start at a random spot
+      currentColumn = Math.floor(Math.random() * dimensions), // our current column - start at a random spot
+      directions = [[-1, 0], [1, 0], [0, -1], [0, 1]], // array to get a random direction from (left,right,up,down)
+      lastDirection = [], // save the last direction we went
+      randomDirection; // next turn/direction - holds a value from directions
+
+    // lets create some tunnels - while maxTunnels, dimentions, and maxLength  is greater than 0.
+    while (maxTunnels && dimensions && maxLength) {
+
+      // lets get a random direction - until it is a perpendicular to our lastDirection
+      // if the last direction = left or right,
+      // then our new direction has to be up or down,
+      // and vice versa
+      do {
+         randomDirection = directions[Math.floor(Math.random() * directions.length)];
+      } while ((randomDirection[0] === -lastDirection[0] && randomDirection[1] === -lastDirection[1]) || (randomDirection[0] === lastDirection[0] && randomDirection[1] === lastDirection[1]));
+
+      var randomLength = Math.ceil(Math.random() * maxLength), //length the next tunnel will be (max of maxLength)
+        tunnelLength = 0; //current length of tunnel being created
+
+		// lets loop until our tunnel is long enough or until we hit an edge
+      while (tunnelLength < randomLength) {
+
+        //break the loop if it is going out of the map
+        if (((currentRow === 0) && (randomDirection[0] === -1)) ||
+            ((currentColumn === 0) && (randomDirection[1] === -1)) ||
+            ((currentRow === dimensions - 1) && (randomDirection[0] === 1)) ||
+            ((currentColumn === dimensions - 1) && (randomDirection[1] === 1))) {
+          break;
+        } else {
+          map[currentRow][currentColumn] = 0; //set the value of the index in map to 0 (a tunnel, making it one longer)
+          currentRow += randomDirection[0]; //add the value from randomDirection to row and col (-1, 0, or 1) to update our location
+          currentColumn += randomDirection[1];
+          tunnelLength++; //the tunnel is now one longer, so lets increment that variable
         }
       }
-      this.setState({
-        gridFull: g2,
-        generation: this.state.generation + 1
-      });
-    }
-// VV dont forget the "O"
-  componentDidMount() {
-    this.seed();
-    this.playButton();
-  }
 
+      if (tunnelLength) { // update our variables unless our last loop broke before we made any part of a tunnel
+        lastDirection = randomDirection; //set lastDirection, so we can remember what way we went
+        maxTunnels--; // we created a whole tunnel so lets decrement how many we have left to create
+      }
+    }
+    return map; // all our tunnels have been created and our map is complete, so lets return it to our render()
+  };
+
+  onClick(e) {
+    this.forceUpdate()
+  }
   render() {
+    let grid = this.createMap();
     return (
-      <div>
-        <h1>Dungeness Crawler</h1>
-        <Buttons
-          playButton={this.playButton}
-          pauseButton={this.pauseButton}
-          seed={this.seed}
-        />
-        <Grid
-          gridFull={this.state.gridFull}
-          rows={this.rows}
-          cols={this.cols}
-          selectBox={this.selectBox}
-        />
-        <h2>Generations: {this.state.generation}</h2>
+      <div >
+        <button  onClick={this.onClick}>tester</button>
+
+        <table className="grid">
+          <thead>
+            {grid.map((obj, row) => <tr key={row}>{obj.map((obj2, col) =>< td className = {
+                obj2 === 1
+                  ? 'wall'
+                  : 'tunnel'
+              }
+              key = {
+                col
+              } > </td>)}</tr>)}
+          </thead>
+        </table>
       </div>
     );
   }
 }
 
-function arrayClone(arr){
-  return JSON.parse(JSON.stringify(arr));
-}
 
-ReactDOM.render(<Main />, document.getElementById('root'));
+ReactDOM.render(<Application />, document.getElementById('app'));
